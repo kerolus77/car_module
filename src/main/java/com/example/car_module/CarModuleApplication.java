e395c186835a3ab8bf4b844ae712b2db8db7c2e6
Mazin Islam <mazinislam431@gmail.com>
2024-04-25 19:09:49 +0200

    Initial commit

---
+++ src/main/java/com/example/car_module/CarModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarModuleApplication.class, args);
+	}
+
+}
